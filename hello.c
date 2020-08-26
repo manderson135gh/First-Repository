08d501f65df6229089fb02c1a9b5ade621070931
Mitch <manderson135@huskers.unl.edu>
2020-08-26 01:54:56 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Mitch Anderson
+ * Date: August 18, 2020
+ * 
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Hello world!\n");
+    
+    return 0;
+}
